f225569c6a8e56bb6422ac6f01e18a83d5fd32dc
Tanishq Srivastava <tanishq280205@gmail.com>
2026-02-12 11:48:00 +0530

    testing local postgresql db connection

--- simple-rag/main.py
+++ simple-rag/main.py
@@ -1,1 +1,18 @@
-from fastapi import UploadFile, HTTPException
+import asyncio
+import asyncpg
+
+db_url= "postgresql://admin:password@localhost:5432/explainit"
+
+async def main():
+    try:
+        conn = await asyncpg.connect(db_url)
+        print("successfully connected to the database")
+        version = await conn.fetchval("SELECT extversion FROM pg_extension WHERE extname = 'vector'")
+        print(f"pgvector version: {version}")
+
+        await conn.close()
+    except Exception as e:
+        print(f"{e}")
+
+if __name__=="__main__":
+    asyncio.run(main())
